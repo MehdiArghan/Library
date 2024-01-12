3d23a03cbcfa892ae9bcb37d1b190ed1e50a0314
mehdi <mehdiarghan@gmail.com.git/>
2024-01-12 11:42:14 +0330

    created BaseEntity class

---
+++ src/main/java/base/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+}
